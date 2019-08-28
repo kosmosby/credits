2d995d0c8a609c1fd5ba3c9c21b9ce14c676b561
kosmos <kosmos.by@gmail.com>
2019-08-28 16:59:44 +0300

    added test mail file #2

--- mail.php
+++ mail.php
@@ -6,5 +6,5 @@ $message = "Line 1\r\nLine 2\r\nLine 3";
 $message = wordwrap($message, 70, "\r\n");
 
 // Отправляем
-mail('kosmos.by@gmail.com', 'My Subject', $message);
+echo mail('kosmos.by@gmail.com', 'My Subject', $message);
 ?>
