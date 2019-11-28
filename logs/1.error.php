278357b4bac066f63e0590ed4443141457b46815
kosmosby <kosmos@dev.effectiff.net>
2019-11-28 08:41:42 +0000

    sites version

---
+++ logs/1.error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-10-23 07:59:41 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-10-23T07:59:40+00:00	INFO 37.144.252.236	cookiefailure	Пустой пароль не допускается
+2019-10-28T13:48:40+00:00	INFO 213.184.244.78	cookiefailure	Пустой пароль не допускается
+2019-11-12T22:10:42+00:00	INFO 46.53.244.78	cookiefailure	Пустой пароль не допускается
---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-11-28 08:10:52 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-11-28T08:10:52+00:00	INFO 194.85.159.34	joomlafailure	Логин или пароль введены неправильно, либо такой учётной записи ещё не существует.
+2019-11-28T08:11:13+00:00	INFO 194.85.159.34	joomlafailure	Логин или пароль введены неправильно, либо такой учётной записи ещё не существует.
